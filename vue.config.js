349eff7ca074385efd1a519191c3fef3c000077e
Xthree Dev <x3develop@gmail.com>
2023-08-22 14:44:05 +0700

    app title

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === "production" ? `/${process.env.VUE_APP_REPO_NAME}/` : "/",
+    chainWebpack: config => {
+        config
+            .plugin('html')
+            .tap(args => {
+                args[0].title = "Bird swap";
+                return args;
+            })
+    }
 };
